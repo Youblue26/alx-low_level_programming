4c336a06f682a5009f8905fcee15d25c2da4dec4
Youblue26 <rakplome@gmail.com>
2023-04-20 13:50:12 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /**
- * main - Entry poit
- * Description: This program prints a message to the console.
- * Return: always 0 (success)
- */
+* main - entry poit
+* Return: always 0 (success)
+*/
 int main(void)
 {
 printf("Programming is like building a multilingual puzzle\n");
