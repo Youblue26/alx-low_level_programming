9de3f6077dd99bfce0c5ad3652474cd850f6f727
Youblue26 <rakplome@gmail.com>
2023-04-20 17:26:52 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+* main prints "and that piece of art is useful" Dora Korpar, 2015-10-19",
+* followed by a newline, to standard error.
+* Return: Always 1.
+*/
+int main(void)I
+{
+write(2, "and that piece of art is usefull" -Dora kopar, 2015-10-19\n", 59);
+return (1);
+}
